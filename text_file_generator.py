a930d98a8469012fa24eb577d91f35e0b8e7df3d
TR-1000 <t.l.ross@outlook.com>
2020-05-16 19:35:29 -0500

    Cleaning up code

--- text_file_generator.py
+++ text_file_generator.py
@@ -13,81 +13,77 @@ url = "https://archiveofourown.org/works/23075947?view_full_work=true"
 
 def save_to_file(url):
     req = requests.get(url)
-
     soup = BeautifulSoup(req.text, "html.parser")
 
-    #Print title
-    title_text = soup.title.text
+    #Get title of story
+    title = soup.title.text.strip()
     invalid_chars = ['<','>',':','"','/',"|",'?','/']
-    title = title_text.strip()
+
     # remove invalid characters from title so it can be used as the file name
     for char in invalid_chars:
         if char in title:
             title = title.replace(char,'')
-    complete_work = open(".\\sample_output\\"+title +".txt", "w", encoding='utf-8')
-    complete_work.write(title + "\n\n")
-    complete_work.write(f'URL: {url} \n')
-    complete_work.write(f'Saved: {date.today()} \n')
+
+    with open(f'.\\output\\{title}.txt', 'w', encoding='utf-8') as text_file:
+        text_file.write(f'{title}\n\n')
+        text_file.write(f'URL: {url} \n')
+        text_file.write(f'Saved: {date.today()} \n')
+
+        #Print stats
+        stats_list = soup.find("dl", {"class": "stats"})
+        stats = stats_list.find_all(["dt","dd"])
+        for stat in stats:
+            text = stat.text.strip()
+            #Put dt and dd tag text on the same line instead of new line
+            if ":" in text:
+                text_file.write(text + "  ")
+
+            else:
+                text_file.write(text + "\n")
 
 
-    #Print stats
-    stats_list = soup.find("dl", {"class": "stats"})
-    stats = stats_list.find_all(["dt","dd"])
-    for stat in stats:
-        text = stat.text.strip()
-        #Put dt and dd tag text on the same line instead of new line
-        if ":" in text:
-            complete_work.write(text + "  ")
-
-        else:
-            complete_work.write(text + "\n")
+        #Remove stats row from metadata, since it has already been printed
+        stats = soup.find_all(class_= "stats")
+        for lines in stats:
+            lines = lines.decompose()
 
 
-
-    #Remove stats row from metadata, since it has already been printed
-    stats = soup.find_all(class_= "stats")
-    for lines in stats:
-        lines = lines.decompose()
-
-
-    #Print meta data
-    meta = soup.select('dl.work.meta.group')[0].find_all(['dt','dd'])
-    for info in meta:
-        text = info.text.strip()
-        #Put dt and dd tag text on the same line instead of new line
-        if ":" in text:
-            complete_work.write(text + "  ")
-        else:
-            complete_work.write(text + "\n")
+        #Print meta data
+        meta = soup.select('dl.work.meta.group')[0].find_all(['dt','dd'])
+        for info in meta:
+            text = info.text.strip()
+            #Put dt and dd tag text on the same line instead of new line
+            if ":" in text:
+                text_file.write(text + "  ")
+            else:
+                text_file.write(text + "\n")
 
 
-    #Print Summary
-    summary = soup.find('div', {'class':'summary'})
-    if summary:
-        text = summary.p.text.strip()
-        complete_work.write(f'\nSummary:\n{text}\n')
+        #Print Summary
+        summary = soup.find('div', {'class':'summary'})
+        if summary:
+            text = summary.p.text.strip()
+            text_file.write(f'\nSummary:\n{text}\n')
 
 
-    #New Line splace
-    new_line = ("\n")
-    complete_work.write(new_line)
+        #New Line splace
+        new_line = ("\n")
+        text_file.write(new_line)
 
 
-    #Main story text
-    chapters = soup.find('div',{'id':'chapters'})
-    tags = chapters.find_all(['h3','p','hr'])
-    for lines in tags:
-        #format spacing
-        if "\n" in lines:
-            #print(lines.text)
-            complete_work.write(lines.text + "\n")
-        else:
-            lines = lines.text + "\n"
-            complete_work.write(lines + "\n")
+        #Main story text
+        chapters = soup.find('div',{'id':'chapters'})
+        tags = chapters.find_all(['h3','p','hr'])
+        for lines in tags:
+            #format spacing
+            if "\n" in lines:
+                #print(lines.text)
+                text_file.write(lines.text + "\n")
+            else:
+                lines = lines.text + "\n"
+                text_file.write(lines + "\n")
 
 
-
-    complete_work.close()
-    print('FINISHED!')
+        print(f'{title} Saved!')
 
 save_to_file(url)
